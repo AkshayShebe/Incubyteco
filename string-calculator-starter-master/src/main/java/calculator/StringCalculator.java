eddd64357a156c332998c37968790a8b95d0a830
D6_49786_AkshayShebe <akshay.shebe@gmail.com>
2021-10-17 19:26:30 +0530

    Project start

---
+++ string-calculator-starter-master/src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+class StringCalculator {
+
+    public int add(String input) {
+    	if(input.isEmpty())
+        return 0;
+    	
+    	return Integer.parseInt(input);
+    }
+}
